6e68def9ac2838eddf5e1b97cf7d2733a3eb0f1d
Idan Lev <idan.93lev@gmail.com>
2021-12-27 00:53:12 +0200

    18-3

--- src/components/18-3/TimeInput.jsx
+++ src/components/18-3/TimeInput.jsx
@@ -1,24 +1,17 @@
 import React, { useState } from "react";
 
-const TimeInput = ({name, handleChange, inputValue}) => {
-  const [value, setValue] = useState('')
-
-  // const handleSubmit = e => {
-  //   if (value === '') {
-  //     return console.log('please add something to do')
-  //   }
-
-  //   createToDoItem(value);
-
-  //   setValue("");
-  // }
-
+const TimeInput = ({ name, handleChange, inputValue }) => {
   return (
     <div>
       {name}
-      <input type="text" name={name} value={inputValue} onChange={handleChange}/>
+      <input
+        type="text"
+        name={name}
+        value={inputValue}
+        onChange={handleChange}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default TimeInput;
